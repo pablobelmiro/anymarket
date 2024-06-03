c18e9f1994a9148fd4c7795d32feea4cf93aee03
Pablo Belmiro <dev.pablobelmiro@gmail.com>
2024-06-02 21:22:15 -0300

    Changelog and Readme added

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+### Added
+
+- v0.0.2 Read me file.
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Anymarket API'
 LONG_DESCRIPTION = 'Anymarket´s API to solve a lot of implementations in your project!'
 
